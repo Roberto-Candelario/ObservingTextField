2dbdba3b8d01eec3066b5045f303b141d4318b28
Roberto Candelario <r-jc@live.com>
2024-08-09 14:19:11 -0400

    Initial Commit

---
+++ ObservingTextField/ObservingTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservingTextFieldApp.swift
+//  ObservingTextField
+//
+//  Created by Roberto Candelario on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ObservingTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ObservingTextFieldTests/ObservingTextFieldTests.swift
@@ -0,0 +1,17 @@
+//
+//  ObservingTextFieldTests.swift
+//  ObservingTextFieldTests
+//
+//  Created by Roberto Candelario on 8/9/24.
+//
+
+import Testing
+@testable import ObservingTextField
+
+struct ObservingTextFieldTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
